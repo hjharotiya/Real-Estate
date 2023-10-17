f413f0f6f48ebc0cb1d82116cafc8df59e3301a5
harshit jharotiya <jharotiyaharshit@gmail.com>
2023-10-17 11:57:31 +0530

    create and run the server

--- client/src/componets/Header.jsx
+++ client/src/componets/Header.jsx
@@ -26,12 +26,16 @@ export default function Header() {
               home
             </li>
           </Link>
-          <li className="hidden sm:inline text-slate-700 hover:underline">
-            About
-          </li>
-          <li className="hidden sm:inline text-slate-700 hover:underline">
-            Sign in
-          </li>
+          <Link to="/about">
+            <li className="hidden sm:inline text-slate-700 hover:underline">
+              About
+            </li>
+          </Link>
+          <Link to="/sign-in">
+            <li className="hidden sm:inline text-slate-700 hover:underline">
+              Sign in
+            </li>
+          </Link>
         </ul>
       </div>
     </header>
