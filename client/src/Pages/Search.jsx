d6bf406d8085b36fc4c08cd4efe605f670d4a04d
harshit jharotiya <jharotiyaharshit@gmail.com>
2023-11-01 15:55:32 +0530

    adding onsubmit and onchange functionallty on search page

--- client/src/Pages/Search.jsx
+++ client/src/Pages/Search.jsx
@@ -1,10 +1,113 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Navigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export default function Search() {
+  const [sidebardata, setSidebardata] = useState({
+    searchTerm: "",
+    type: "all",
+    parking: false,
+    furnished: false,
+    offer: false,
+    sort: "created_at",
+    order: "desc",
+  });
+
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+  const [listing, setListing] = useState([]);
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFormUrl = urlParams.get("searchTerm");
+    const typeFormUrl = urlParams.get("type");
+    const parkingFormUrl = urlParams.get("parking");
+    const furnishedFormUrl = urlParams.get("furnished");
+    const offerFormUrl = urlParams.get("offer");
+    const sortFormUrl = urlParams.get("sort");
+    const orderFormUrl = urlParams.get("order");
+
+    if (
+      searchTermFormUrl ||
+      typeFormUrl ||
+      typeFormUrl ||
+      parkingFormUrl ||
+      furnishedFormUrl ||
+      offerFormUrl ||
+      sortFormUrl ||
+      orderFormUrl
+    ) {
+      setSidebardata({
+        searchTerm: searchTermFormUrl || "",
+        type: typeFormUrl || "all",
+        parking: parkingFormUrl === "true" ? true : false,
+        furnished: furnishedFormUrl === "true" ? true : false,
+        offer: offerFormUrl === "true" ? true : false,
+        sort: sortFormUrl || "created_at",
+        order: orderFormUrl || "desc",
+      });
+    }
+
+    const fetchListing = async () => {
+      setLoading(true);
+      const searchQuery = urlParams.toString();
+      const res = await fetch(`api/listing/get?${searchQuery}`);
+      const data = await res.json();
+      setListing(data);
+      setLoading(false);
+    };
+    fetchListing();
+  }, [location.search]);
+  console.log(listing);
+
+  const handelChange = (e) => {
+    if (
+      e.target.id === "all" ||
+      e.target.id === "rent" ||
+      e.target.id === "sale"
+    ) {
+      setSidebardata({ ...sidebardata, type: e.target.id });
+    }
+    if (e.target.id === "searchTerm") {
+      setSidebardata({ ...sidebardata, searchTerm: e.target.value });
+    }
+    if (
+      e.target.id === "parking" ||
+      e.target.id === "furnished" ||
+      e.target.id === "offer"
+    ) {
+      setSidebardata({
+        ...sidebardata,
+        [e.target.id]:
+          e.target.checked || e.target.checked === "true" ? true : false,
+      });
+    }
+
+    if (e.target.id === "sort_order") {
+      const sort = e.target.value.split("_")[0] || "created_at";
+      const order = e.target.value.split("_")[1] || "desc";
+
+      setSidebardata({ ...sidebardata, sort, order });
+    }
+  };
+
+  const handelSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams();
+    urlParams.set("searchTerm", sidebardata.searchTerm);
+    urlParams.set("type", sidebardata.type);
+    urlParams.set("offer", sidebardata.offer);
+    urlParams.set("sort", sidebardata.sort);
+    urlParams.set("parking", sidebardata.parking);
+    urlParams.set("furnished", sidebardata.furnished);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
   return (
     <div className="flex flex-col md:flex-row">
       <div className=" p-7 border-b-2 md:border-r-2 md:min-h-screen">
-        <form className=" flex flex-col gap-8">
+        <form className=" flex flex-col gap-8" onSubmit={handelSubmit}>
           <div className="flex items-center gap-2">
             <label className="font-semibold whitespace-nowrap">
               Search Term:
@@ -14,6 +117,8 @@ export default function Search() {
               id="searchTerm"
               placeholder="Search..."
               className="border rounded-lg p-3 w-full"
+              value={sidebardata.searchTerm}
+              onChange={handelChange}
             />
           </div>
           {/* ***************** type  **************  */}
@@ -21,20 +126,44 @@ export default function Search() {
           <div className="flex gap-2 flex-wrap items-center">
             <label className="font-semibold">Type:</label>
             <div className="flex gap-2">
-              <input type="checkbox" id="all" className="w-5" />
+              <input
+                type="checkbox"
+                id="all"
+                className="w-5"
+                onChange={handelChange}
+                checked={sidebardata.type === "all"}
+              />
               <span>Rent & Sale</span>
             </div>
             <div className="flex gap-2">
-              <input type="checkbox" id="Rent" className="w-5" />
+              <input
+                type="checkbox"
+                id="rent"
+                className="w-5"
+                onChange={handelChange}
+                checked={sidebardata.type === "rent"}
+              />
               <span>Rent</span>
             </div>
             <div className="flex gap-2">
-              <input type="checkbox" id="sale" className="w-5" />
+              <input
+                type="checkbox"
+                id="sale"
+                className="w-5"
+                onChange={handelChange}
+                checked={sidebardata.type === "sale"}
+              />
               <span>Sale</span>
             </div>
 
             <div className="flex gap-2">
-              <input type="checkbox" id="offer" className="w-5" />
+              <input
+                type="checkbox"
+                id="offer"
+                className="w-5"
+                onChange={handelChange}
+                checked={sidebardata.offer}
+              />
               <span>Offer</span>
             </div>
           </div>
@@ -44,25 +173,38 @@ export default function Search() {
           <div className="flex gap-2 flex-wrap items-center">
             <label className="font-semibold">Amenities:</label>
             <div className="flex gap-2">
-              <input type="checkbox" id="parking" className="w-5" />
+              <input
+                type="checkbox"
+                id="parking"
+                className="w-5"
+                onChange={handelChange}
+                checked={sidebardata.parking}
+              />
               <span>parking</span>
             </div>
             <div className="flex gap-2">
-              <input type="checkbox" id="furnished" className="w-5" />
+              <input
+                type="checkbox"
+                id="furnished"
+                className="w-5"
+                onChange={handelChange}
+                checked={sidebardata.furnished}
+              />
               <span>Furnished</span>
-            </div>
-            <div className="flex gap-2">
-              <input type="checkbox" id="offer" className="w-5" />
-              <span>Offer</span>
             </div>
           </div>
           <div className="border rounded-lg p-3">
             <label className="font-semibold">Sort :</label>
-            <select name="" id="sort_order">
-              <option value="">Price high to low</option>
-              <option value="">Price low to high</option>
-              <option value="">latest</option>
-              <option value="">Oldest</option>
+            <select
+              onChange={handelChange}
+              name=""
+              id="sort_order"
+              defaultValue={"created_at_desc"}
+            >
+              <option value="regularPrice_desc">Price high to low</option>
+              <option value="regularPrice_asc">Price low to high</option>
+              <option value="createdAt_desc">latest</option>
+              <option value="createdAt_asc">Oldest</option>
             </select>
           </div>
           <button className="bg-slate-700 uppercase text-white rounded-lg p-2 hover:opacity-95">
