1bb7afb79901442eb4824cdf62e177892b98cb25
harshit jharotiya <jharotiyaharshit@gmail.com>
2023-11-04 09:20:50 +0530

    fixing in signUp.jsx OAth component

--- client/src/Pages/SignUp.jsx
+++ client/src/Pages/SignUp.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import OAuth from "../componets/Oauth";
+import OAuth from "../componets/OAuth";
 
 const SignUp = () => {
   const [formData, setFormData] = useState({});
